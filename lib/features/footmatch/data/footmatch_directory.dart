c959b5e4ac429dfdeecd40cabcecce34362d810b
qlrxn1152 <155734099+qlrxn1152@users.noreply.github.com>
2026-09-19 21:39:28 +0900

    feat: consume member and team ids

--- lib/features/footmatch/data/footmatch_directory.dart
+++ lib/features/footmatch/data/footmatch_directory.dart
@@ -1,14 +1,17 @@
 class FootmatchMemberListItem {
   const FootmatchMemberListItem({
+    required this.id,
     required this.username,
     required this.rating,
   });
 
+  final int id;
   final String username;
   final int rating;
 
   factory FootmatchMemberListItem.fromJson(Map<String, dynamic> json) {
     return FootmatchMemberListItem(
+      id: (json['id'] as num).toInt(),
       username: json['username'] as String,
       rating: (json['rating'] as num).toInt(),
     );
@@ -17,12 +20,14 @@ class FootmatchMemberListItem {
 
 class FootmatchTeamListItem {
   const FootmatchTeamListItem({
+    required this.id,
     required this.teamName,
     required this.teamRating,
     required this.leaderUsername,
     required this.memberCount,
   });
 
+  final int id;
   final String teamName;
   final int teamRating;
   final String leaderUsername;
@@ -30,6 +35,7 @@ class FootmatchTeamListItem {
 
   factory FootmatchTeamListItem.fromJson(Map<String, dynamic> json) {
     return FootmatchTeamListItem(
+      id: (json['id'] as num).toInt(),
       teamName: json['teamName'] as String,
       teamRating: (json['teamRating'] as num).toInt(),
       leaderUsername: json['leaderUsername'] as String,
